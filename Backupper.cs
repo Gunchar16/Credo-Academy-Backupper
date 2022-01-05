b42064ca03daebf002206bcf2c2041ba39c0c3cf
Gunchar <Gunchar@DESKTOP-PUDR2K0>
2022-01-05 17:52:09 +0400

    Add project files.

---
+++ Backupper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backup
+{
+    public class Backupper
+    {
+        public static void Backup(string srcPath, string destPath, DateTime time)
+        {
+            if (!Directory.Exists(srcPath) || !Directory.Exists(destPath))
+            {
+                Console.WriteLine("One of the paths specified do not exist, please re-check and try again later");
+                return;
+            }
+            foreach (var file in Directory.GetFiles(srcPath))
+                if (File.GetCreationTime(file) > time)
+                    File.Copy(file, Path.Combine(destPath, Path.GetFileName(file)), true);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+
+namespace Backup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time;
+            try
+            {
+                time = DateTime.ParseExact(args[0], "d-M-yyyy", new CultureInfo("ka-GE"));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+            Backupper.Backup(args[1], args[2], time);
+        }
+    }
+}
